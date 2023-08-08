ebb8fa990665d6d19a650b139cb83b6f647614ec
Ibrat Nawaz <nawazmd0096@gmail.com>
2023-08-08 19:58:18 +0530

    feat: update page

--- src/App.js
+++ src/App.js
@@ -1,196 +1,48 @@
 import React from 'react';
+import './App.css';
 
 const App = () => {
 	return (
-		<div>
-			<Greeting time='morning' />
-			<Hello name='John'/>
-			<h1>
-				Welcome to React App thats build from scratch manually using Webpack and
-				Babel
-			</h1>
-			<div class='bg-white'>
-				<header class='absolute inset-x-0 top-0 z-50'>
-					<nav
-						class='flex items-center justify-between p-6 lg:px-8'
-						aria-label='Global'>
-						<div class='flex lg:flex-1'>
-							<a href='#' class='-m-1.5 p-1.5'>
-								<span class='sr-only'>Your Company</span>
-								<img
-									class='h-8 w-auto'
-									src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-									alt=''
-								/>
-							</a>
-						</div>
-						<div class='flex lg:hidden'>
-							<button
-								type='button'
-								class='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'>
-								<span class='sr-only'>Open main menu</span>
-								<svg
-									class='h-6 w-6'
-									fill='none'
-									viewBox='0 0 24 24'
-									stroke-width='1.5'
-									stroke='currentColor'
-									aria-hidden='true'>
-									<path
-										stroke-linecap='round'
-										stroke-linejoin='round'
-										d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-									/>
-								</svg>
-							</button>
-						</div>
-						<div class='hidden lg:flex lg:gap-x-12'>
-							<a href='#' class='text-sm font-semibold leading-6 text-gray-900'>
-								Product
-							</a>
-							<a href='#' class='text-sm font-semibold leading-6 text-gray-900'>
-								Features
-							</a>
-							<a href='#' class='text-sm font-semibold leading-6 text-gray-900'>
-								Marketplace
-							</a>
-							<a href='#' class='text-sm font-semibold leading-6 text-gray-900'>
-								Company
-							</a>
-						</div>
-						<div class='hidden lg:flex lg:flex-1 lg:justify-end'>
-							<a href='#' class='text-sm font-semibold leading-6 text-gray-900'>
-								Log in <span aria-hidden='true'>&rarr;</span>
-							</a>
-						</div>
-					</nav>
-
-					<div class='lg:hidden' role='dialog' aria-modal='true'>
-						<div class='fixed inset-0 z-50'></div>
-						<div class='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
-							<div class='flex items-center justify-between'>
-								<a href='#' class='-m-1.5 p-1.5'>
-									<span class='sr-only'>Your Company</span>
-									<img
-										class='h-8 w-auto'
-										src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-										alt=''
-									/>
-								</a>
-								<button
-									type='button'
-									class='-m-2.5 rounded-md p-2.5 text-gray-700'>
-									<span class='sr-only'>Close menu</span>
-									<svg
-										class='h-6 w-6'
-										fill='none'
-										viewBox='0 0 24 24'
-										stroke-width='1.5'
-										stroke='currentColor'
-										aria-hidden='true'>
-										<path
-											stroke-linecap='round'
-											stroke-linejoin='round'
-											d='M6 18L18 6M6 6l12 12'
-										/>
-									</svg>
-								</button>
-							</div>
-							<div class='mt-6 flow-root'>
-								<div class='-my-6 divide-y divide-gray-500/10'>
-									<div class='space-y-2 py-6'>
-										<a
-											href='#'
-											class='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-											Product
-										</a>
-										<a
-											href='#'
-											class='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-											Features
-										</a>
-										<a
-											href='#'
-											class='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-											Marketplace
-										</a>
-										<a
-											href='#'
-											class='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-											Company
-										</a>
-									</div>
-									<div class='py-6'>
-										<a
-											href='#'
-											class='-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
-											Log in
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
+		<>
+			<nav>
+				<div className='menu'>
+					<div className='logo'>
+						<a href='#'>CodingLab</a>
 					</div>
-				</header>
-
-				<div class='relative isolate px-6 pt-14 lg:px-8'>
-					<div
-						class='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
-						aria-hidden='true'>
-						<div
-							class='clip-path relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'>
-
-							</div>
-					</div>
-					<div class='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
-						<div class='hidden sm:mb-8 sm:flex sm:justify-center'>
-							<div class='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
-								Announcing our next round of funding.{' '}
-								<a href='#' class='font-semibold text-indigo-600'>
-									<span class='absolute inset-0' aria-hidden='true'></span>Read
-									more <span aria-hidden='true'>&rarr;</span>
-								</a>
-							</div>
-						</div>
-						<div class='text-center'>
-							<h1 class='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
-								Data to enrich your online business
-							</h1>
-							<p class='mt-6 text-lg leading-8 text-gray-600'>
-								Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-								lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-								fugiat aliqua.
-							</p>
-							<div class='mt-10 flex items-center justify-center gap-x-6'>
-								<a
-									href='#'
-									class='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
-									Get started
-								</a>
-								<a
-									href='#'
-									class='text-sm font-semibold leading-6 text-gray-900'>
-									Learn more <span aria-hidden='true'>→</span>
-								</a>
-							</div>
-						</div>
-					</div>
-					<div
-						class='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'
-						aria-hidden='true'>
-						<div
-							class='clip-path relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'>
-
-							</div>
-					</div>
+					<ul>
+						<li>
+							<a href='#'>Home</a>
+						</li>
+						<li>
+							<a href='#'>About</a>
+						</li>
+						<li>
+							<a href='#'>Services</a>
+						</li>
+						<li>
+							<a href='#'>Contact</a>
+						</li>
+						<li>
+							<a href='#'>Feedback</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<div className='img'></div>
+			<div className='center'>
+				<div className='title'><Hello name='John'/></div>
+				<div className='sub_title'><Greeting time='afternoon'/></div>
+				<div className='btns'>
+					<button>Learn More</button>
+					<button>Subscribe</button>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
-
 function Hello(props) {
+	if (props.name == '') throw new Error('Please provide a name');
 	return <h1>Hello {props.name}</h1>;
 }
 
